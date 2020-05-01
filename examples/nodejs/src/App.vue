842a57d1646052058f54a5e94505f3f486b13708
Ruslan Makarov <ruslan.makarov@gmail.com>
2020-05-01 21:53:13 +0700

    CSS import

--- examples/nodejs/src/App.vue
+++ examples/nodejs/src/App.vue
@@ -31,7 +31,9 @@ export default {
 };
 </script>
 
-<style>	
+<style>
+	@import '~@vuesence/book/src/css/default.css';
+	@import '~@vuesence/book/src/css/vuepress-style.css';
 	/* @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');	 */
 	/* @import './css/default.css'; */
 	/* @import './vuepress-style.css';	 */
